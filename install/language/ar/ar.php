3bdf0880779de4549c977ee373c24f8affd36d75
mousa ageeli <mousaageeli@gmail.com>
2017-05-15 04:29:17 +0300

    remove install

--- install/language/ar/ar.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Button
-$_['button_continue'] = 'متابعة';
-$_['button_back']     = 'رجوع';
-
-// Error
-$_['error_exception'] = 'رمز الخطأ(%s): %s  في السطر %s رقم %s';
--- install/language/ar/common/footer.php
+++
@@ -1,6 +0,0 @@
-<?php
-$_['text_project']       = 'موقع أوبن كارت';
-$_['text_project_arab']  = 'موقع أوبن كارت العرب';
-$_['text_documentation'] = 'تعليمات';
-$_['text_support']       = 'منتدى الدعم';
-$_['text_footer']        = '2014 ©  OpenCart - جميع الحقوق محفوظة';
